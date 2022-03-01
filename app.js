0462d06a2508f1e3d80827ece770ba6c3c88eb8e
Vitor Martins <martinsvitor@gmx.de>
2022-03-01 19:58:28 +0100

    Some changes to routes, middleware, authentication

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
+require("./config/session.config")(app);
 
 // default value for title local
 const capitalized = require("./utils/capitalized");
@@ -35,5 +36,6 @@ const gameRoutes = require('./routes/game.routes');
 app.use('/game', gameRoutes);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
 
 module.exports = app;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,13 +5,30 @@ const session = require('express-session');
 
 // Internal imports
 const User = require('../models/User.model');
+const { isLoggedIn } = require('../middleware/route-guard.js');
 
 const saltRounds = 10;
 
-// Get Routes
-router.get('/signup', (req, res) => res.render('auth/signup'));
 
-router.get('/login', (req, res) => res.render('auth/login'));
+
+// Get Routes
+router.get('/signup', (req, res, next) => {
+    res.render('auth/signup')
+});
+
+router.get('/login', isLoggedIn, (req, res, next) => {
+    res.render('auth/login')
+});
+
+router.get('/myaccount', isLoggedIn, (req, res, next) => {
+    res.render('auth/my-account')
+});
+
+router.get('/logout', isLoggedIn, (req, res, next) => {
+    req.session.destroy();
+    // res.write('Redirecting to home in 5 seconds...');
+    setTimeout(() => res.redirect('/'), 5000)
+});
 
 // Post Routes
 
@@ -25,14 +42,31 @@ router.post('/signup', (req, res, next) => {
                 password: hashed,
                 email
             });
-            res.render('/')
+            res.redirect('/')
         })
         .catch(error => console.log(`This error occured while creating user: ${error}`));
 
 });
 
 router.post('/login', (req, res, next) => {
-    console.log(req.session)
+    const { username, password } = req.body
+    User.findOne({username})
+        .then(user => {
+            if(!user){
+                return res.render('auth/login',  { errorMessage : 'Username/password incorrect or  user does not exist' });
+            }
+            else if(bcrypt.compare(password, user.password)){
+                // console.log('Autorization granted', user);
+                req.session.currentUser = user;
+                // console.log(req.session)
+                return res.redirect('/');
+            }
+            else{
+                return res.render('auth/login',  { errorMessage : 'Username/password incorrect or  user does not exist' });
+            }
+        })
+        .catch(error => console.log('Error during user authentication', error));
+        
 });
 
 // Exports
