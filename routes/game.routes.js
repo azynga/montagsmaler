bdf40291262f15be4ab35229c0a3b77b2d3a04c5
Aljoscha Zynga <aljoscha.zynga@gmail.com>
2022-03-04 03:50:12 +0100

    add basic game list page, add game page with basic drawing and mirroring functionality

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -10,55 +10,76 @@ const { isLoggedIn } = require('../middleware/route-guard.js');
 router.get('/matchlist', isLoggedIn, (req, res, next) => {
     if(req.session){
         const {currentUser} = req.session
-        res.render('game/matchlist', { currentUser });
+        res.render('game/list', { allGames, currentUser });
         // console.log(req.session.currentUser)
-      }
+    }
     // res.render('game/matchlist');
 });
 
 router.get('/create', (req, res) => {
     const userId = req.session.currentUser['_id'];
     const game = new Game();
-    game.addPlayer(userId);
     allGames[game.gameId] = game;
-    res.redirect('/game/:gameId');
+    res.redirect(`/game/${game.gameId}`);
 });
 
+
 router.get('/:gameId', (req, res) => {
-    const {currentUser} = req.session;
     const { gameId } = req.params;
-    const game = allGames.find((game) => game.gameId === gameId);
-    const playerNames = game.players.map(player => {
-        User.findById(player.userId)
-            .then(playerName => playerName)
-            .catch(error => console.error(error));
-    });
-    res.render('game/game', { players, currentUser });
+    
+    if(!allGames[gameId]) {
+        res.send('This game ID does not exist.');
+        
+    } else {
+        const { currentUser } = req.session;
+        const userId = currentUser['_id'];
+        const game = allGames[gameId];
+        game.addPlayer(userId);
+        const players = game.players;
+        res.render('game/game', { players, currentUser, gameId });
+    };
+});
+
+router.get('/:gameId/leave', (req, res) => {
+    const { gameId } = req.params;
+    const userId = req.session.currentUser['_id'];
+    const game = allGames[gameId];
+    game.removePlayer(userId);
+    res.redirect('/game/matchlist');
 });
 
 router.get('/:gameId/data', (req, res) => {
     const { gameId } = req.params;
+    const userId = req.session.currentUser['_id'];
+    const game = allGames[gameId];
+    const players = game.players;
+    const isPlayerDrawing = players[game.drawingPlayerIndex].userId === userId;
     const drawingData = allGames[gameId].currentDrawingData;
-    res.send(drawingData);
-});
 
-// router.get(':gameId/lobby', (req, res) => {
-//     const { currentUser } = req.session;
-//     const { gameId } = req.params;
-//     const players = gameList[gameId].players;
-//     gameList[gameId].players.push(currentUser);
-//     res.render('games/lobby', { players });
-// });
-
-    gameList[gameId].players.push(currentUser);
-    res.render('games/lobby', { currentUser, players });
+    const data = {
+        drawingData,
+        players,
+        isPlayerDrawing
+    }
+    
+    res.send(data);
+    
 });
 
 router.post('/:gameId/data', (req, res) => {
+    const drawingData = req.body;
     const { gameId } = req.params;
-    const drawingData = allGames[gameId].currentDrawingData;
-    drawingData.lines = req.body.lines;
-    res.end();
+    const userId = req.session.currentUser['_id'];
+    const game = allGames[gameId];
+    game.currentDrawingData = drawingData;
+    const players = game.players;
+    const isPlayerDrawing = players[game.drawingPlayerIndex].userId === userId;
+
+    const data = {
+        players,
+        isPlayerDrawing
+    }
+    res.send(data);
 });
 
 module.exports = router;
