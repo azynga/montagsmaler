51a4f34e87507681711653ef7da5900f6c0ff8eb
Vitor Martins <martinsvitor@gmx.de>
2022-03-15 21:13:05 +0100

    fix expanding images

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,29 +1,22 @@
 // Expanding images
 document.querySelectorAll('.drawing').forEach(drawing => {
-        let isExpanded = false;
-        const expandedImg = document.getElementById('expanded-img');
-        const overlayDiv = document.getElementById('overlay');
+    const expandedImg = document.getElementById('expanded-img');
+    const overlayDiv = document.getElementById('overlay');
     drawing.onclick = () => {
-        if(!isExpanded){
-            overlayDiv.classList.add('overlay');
-            isExpanded = true;
-            expandedImg.src = drawing.src;
-            expandedImg.alt = drawing.alt;
-            console.log(expandedImg, isExpanded);
-        };
+        overlayDiv.classList.add('overlay');
+        expandedImg.src = drawing.src;
+        expandedImg.alt = drawing.alt;
+        console.log(expandedImg);
     };
-    if(!isExpanded){
-        overlayDiv.onclick = () => {
-            overlay.classList.remove('overlay');
-            expandedImg.src ='';
-            expandedImg.alt ='';
-            isExpanded = false;
-        };
+    overlayDiv.onclick = () => {
+        overlay.classList.remove('overlay');
+        expandedImg.src = '';
+        expandedImg.alt = '';
     };
 });
 document.querySelectorAll('.delete-button').forEach(button => {
-button.onclick = () => confirm('Do you really want to delete this drawing? It can\'t be undone.')
+    button.onclick = () => confirm('Do you really want to delete this drawing? It can\'t be undone.')
 });
 document.querySelectorAll('.publish-button').forEach(button => {
-button.onclick = () => confirm('Do you really want to publish this drawing? It can\'t be undone.')
+    button.onclick = () => confirm('Do you really want to publish this drawing? It can\'t be undone.')
 });
