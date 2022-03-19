a8493422c45980bdfe68ec0e59681c4a78bed8e2
Vitor Martins <94311178+martinsvitor@users.noreply.github.com>
2022-03-19 10:29:10 +0100

    Update session.config.js

--- config/session.config.js
+++ config/session.config.js
@@ -19,7 +19,7 @@ module.exports = app => {
           sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
           secure: process.env.NODE_ENV === 'production',
           httpOnly: true,
-          maxAge: 1000 * 60 * 60 * 24 // 60 * 1000 ms === 1 min
+          // maxAge: 1000 * 60 * 60 * 24 // 60 * 1000 ms === 1 min
         },
         store: MongoStore.create({
           mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/basic-auth',
